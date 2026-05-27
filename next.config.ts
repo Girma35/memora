a0ad71834e493a3a7413fd2a66e5c25d26024e19
girma35 <girmawakeyo4@gmail.com>
2026-05-27 11:16:42 +0300

    feat: configure turbopack root directory in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import type { NextConfig } from "next";
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname
+  }
 };
 
 export default nextConfig;
